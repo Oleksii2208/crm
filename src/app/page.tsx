8160a257e49295da72c9a539f723789cf5db474b
Oleksiy Kost <AlekseyKo2208@gmail.com>
2025-10-13 05:42:51 +0300

    Add error handling components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AddCompanyButton from './components/add-company-button';
+import MagicButton from './components/magic-button';
 // import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
@@ -7,10 +8,7 @@ export default function Home() {
       <main>
         <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
         <AddCompanyButton />
-        {/* <StatusLabel status={Status.Active}>Active</StatusLabel>
-        <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
-        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel> */}
+        <MagicButton />
       </main>
     </div>
   );
