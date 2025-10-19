0423ad13b51b58e8a5a5fa08a0233d0a2325d44d
Oleksiy Kost <AlekseyKo2208@gmail.com>
2025-10-19 22:34:10 +0300

    Add new company page

---
+++ src/app/(admin)/companies/@toolbar/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '@/app/components/header';
+import SearchInput from '@/app/components/search-input';
+import AddCompanyButton from '@/app/components/add-company-button';
+import Toolbar from '@/app/components/toolbar';
+
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+  return (
+    <Toolbar action={<AddCompanyButton />}>
+      <SearchInput />
+    </Toolbar>
+  );
+}
